60a18e1352fa3d4d85195244ae35500ec4c167c1
spencerwb <websp@seas.upenn.edu>
2020-10-21 13:53:40 -0400

    test

--- Projects/mass_spring/SimulationDriver.h
+++ Projects/mass_spring/SimulationDriver.h
@@ -65,8 +65,8 @@ public:
         // TODO: update position and velocity according to Newton's law.
         for (int i = 0; i < n; i++) {
             if (!ms.node_is_fixed.at(i)) {
-                ms.x.at(i) += (dt * ms.v.at(i));
-                ms.v.at(i) += (dt * (gravity + f_spring.at(i) + f_damping.at(i)) / ms.m.at(i));
+                // ms.x.at(i) += (dt * ms.v.at(i));
+                // ms.v.at(i) += (dt * (gravity + f_spring.at(i) + f_damping.at(i)) / ms.m.at(i));
 
 
                 ms.x.at(i) += TV(0.f, 0.f, dt * 50.f);
